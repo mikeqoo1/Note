5489c06e73195c26e41b68c2ba6414df84bcae7a
seaflower <mikeqoo123@gmail.com>
2025-11-19 15:46:42 +0800

    更新

--- uNetworking/MyApp/README.md
+++ uNetworking/MyApp/README.md
@@ -1,5 +1,19 @@
-# 說明
+# 怎麼執行
 
 1. npm i
 2. node test-uws.js
 3. curl http://localhost:9001/
+
+# 怎麼測試
+
+我是用一個有編譯安裝好curl http3 的 docker
+
+sudo docker run --rm --network host ymuski/curl-http3 curl --http3 -k -v https://127.0.0.1:9001/
+
+![alt text](image.png)
+
+![alt text](image-1.png)
+
+那個錯誤原因在這邊
+
+![alt text](image-2.png)
--- uNetworking/MyApp/test-uws.js
+++ uNetworking/MyApp/test-uws.js
@@ -1,16 +1,24 @@
-// test-uws.js
 const uWS = require('@mike/uws-http3');
 
 console.log('Node modules ABI =', process.versions.modules);
+const port = 9001;
 
-uWS.App()
-  .get('/', (res, req) => {
-    res.end('Hello from custom uWS HTTP/3 build!');
-  })
-  .listen(9001, (token) => {
-    if (token) {
-      console.log('✅ Listening on port 9001');
-    } else {
-      console.error('❌ Failed to listen on port 9001');
-    }
-  });
+const app = uWS.H3App({
+  key_file_name: 'misc/key.pem',
+  cert_file_name: 'misc/cert.pem',
+  passphrase: '1234'
+});
+
+console.log('H3App created:', typeof app, Object.keys(app));
+
+app.get('/*', (res, req) => {
+  res.end('H3llo World!');
+});
+
+app.listen(port, (token) => {
+  if (token) {
+    console.log('✅ H3 server listening on UDP port', port);
+  } else {
+    console.log('❌ Failed to listen on port', port);
+  }
+});
